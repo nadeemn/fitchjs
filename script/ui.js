308df9072540878c5c6d2d5961a09cc01a43ead4
nadeemn <nadeemnazer123@gmail.com>
2024-09-27 01:12:38 +0200

    changed ui style.

--- script/ui.js
+++ script/ui.js
@@ -8,14 +8,14 @@ function disp(id) {
 		document.getElementsByClassName('buttons')[0].style.display='block';
 	}
 	for(var i=0;i<menu_items.length;i++) {
-		document.getElementById(menu_items[i]).style.backgroundColor = '#DDDDDD';
+		document.getElementById(menu_items[i]).style.backgroundColor = '#002A4E';
 	}
 	for(var i=0;i<menu_contents.length;i++) {
 		document.getElementById(menu_contents[i]).style.display='none';
 	}
 	var show_table = (id=='app' && !proof_started) ? 'prbt' : id+'t';
 	document.getElementById(show_table).style.display = 'block';
-	document.getElementById(id+'m').style.backgroundColor = 'white';
+	document.getElementById(id+'m').style.backgroundColor = '#002A4E';
 }
 
 function show(id) {
